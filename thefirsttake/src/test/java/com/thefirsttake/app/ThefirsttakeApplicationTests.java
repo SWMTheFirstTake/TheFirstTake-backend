b2aa9b3c5cc9074b1e788a09ca9ee54d05b60629
Hyeonseong0917 <sagiri0917@gmail.com>
2025-06-25 18:43:13 +0900

    test

--- thefirsttake/src/test/java/com/thefirsttake/app/ThefirsttakeApplicationTests.java
+++ thefirsttake/src/test/java/com/thefirsttake/app/ThefirsttakeApplicationTests.java
@@ -2,8 +2,10 @@ package com.thefirsttake.app;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")  // 테스트 시 'test' 프로파일 활성화
 class ThefirsttakeApplicationTests {
 
 	@Test
