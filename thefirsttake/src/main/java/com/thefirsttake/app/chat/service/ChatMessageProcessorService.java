b811efd3e279fd63aacca8251175b9dd5c5b50da
Hyeonseong0917 <sagiri0917@gmail.com>
2025-07-03 12:06:23 +0900

    feat: AI별 특징 추가

--- thefirsttake/src/main/java/com/thefirsttake/app/chat/service/ChatMessageProcessorService.java
+++ thefirsttake/src/main/java/com/thefirsttake/app/chat/service/ChatMessageProcessorService.java
@@ -16,18 +16,23 @@ public class ChatMessageProcessorService {
     public List<String> generateCurationResponse(String userInput, Long roomId) {
         String promptKey=roomId+":prompt";
         // 1. redis에 promptKey를 key로 프롬프트를 가져오거나 or 새로운 프롬프트 생성
-        String currentPromptValue=promptValueService.getPrompt(promptKey,userInput);
+        String currentPromptValue=promptValueService.getPrompt(promptKey,userInput,1);
 
         // 2. 프롬프트를 바탕으로 큐레이션 결과를 가져옴
         // 첫 번째 AI
         String curationResult=curationResultService.getResult(promptKey,currentPromptValue);
         curationResult+="1번째 AI";
         // 두 번째 AI
+        currentPromptValue=promptValueService.getPrompt(promptKey,userInput,2);
         String tmpCurationResult=curationResultService.getResult(promptKey,currentPromptValue);
         tmpCurationResult+="2번째 AI";
 
+        // 세 번째 AI
+        currentPromptValue=promptValueService.getPrompt(promptKey,userInput,3);
+        String cityBoyCurationResult=curationResultService.getResult(promptKey,currentPromptValue);
+        cityBoyCurationResult+="3번째 AI"
         // 3. 프롬프트밸류+현재 큐레이션 결과를 저장
-        promptCacheService.savePrompt(promptKey,currentPromptValue+curationResult+tmpCurationResult);
+        promptCacheService.savePrompt(promptKey,currentPromptValue+curationResult+tmpCurationResult+currentPromptValue);
 
         // 4. 큐레이션 결과를 사용자에게 보여주기 위해 보여줌
         List<String> curationResults=new ArrayList<>();
