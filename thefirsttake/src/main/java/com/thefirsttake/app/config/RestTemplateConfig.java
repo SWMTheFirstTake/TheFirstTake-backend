5091276166becafa4e324b73d6663313a626c238
Hyeonseong0917 <sagiri0917@gmail.com>
2025-08-04 14:14:41 +0900

    feat: 연결 타임아웃 증가

--- thefirsttake/src/main/java/com/thefirsttake/app/config/RestTemplateConfig.java
+++ thefirsttake/src/main/java/com/thefirsttake/app/config/RestTemplateConfig.java
@@ -11,8 +11,8 @@ public class RestTemplateConfig {
     @Bean
     public RestTemplate restTemplate() {
         SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
-        factory.setConnectTimeout(10000); // 연결 타임아웃 5초
-        factory.setReadTimeout(10000);    // 읽기 타임아웃 5초
+        factory.setConnectTimeout(20000); // 연결 타임아웃 5초
+        factory.setReadTimeout(20000);    // 읽기 타임아웃 5초
         
         return new RestTemplate(factory);
     }
